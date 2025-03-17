efe1ad124d8666c237d8ffbba7546ebc6c2b0d1a
AryaGGWP <arya.lampung.2016@gmail.com>
2025-03-17 19:08:29 +0700

    add library, add variabel, add parameter

---
+++ contohlooping.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int counter;
+    int i;
+    int perulanganwhile = 0;
+    int perulanganDo = 0;
+
+    //loop with for
+    //loop for ascending
+    for(counter = 0; counter < 5; counter++) {
+        cout << "Nilai Counter = " << counter << "TI UMY" << endl;
+    }
+    cout << "Nilai counter terakhir = " << counter << endl;
+    cout <<endl;
+}
