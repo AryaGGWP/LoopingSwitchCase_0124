f73a5209625849a2388b2b60d1e512c201146062
AryaGGWP <arya.lampung.2016@gmail.com>
2025-03-17 19:17:09 +0700

    create looping for decrement

--- contohlooping.cpp
+++ contohlooping.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main() {
+    //deklarasi variabel
     int counter;
     int i;
     int perulanganwhile = 0;
@@ -12,6 +13,13 @@ int main() {
     //loop with for
     //loop for ascending
     for(counter = 0; counter < 5; counter++) {
+        cout << "Nilai Counter = " << counter << "TI UMY" << endl;
+    }
+    cout << "Nilai counter terakhir = " << counter << endl;
+    cout <<endl;
+
+    //loop for descending
+    for(counter = 5; counter > 0; counter--) {
         cout << "Nilai Counter = " << counter << "TI UMY" << endl;
     }
     cout << "Nilai counter terakhir = " << counter << endl;
