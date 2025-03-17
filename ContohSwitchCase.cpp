5176c5607cde639ef1280b40a0fe286e3d76a2d7
AryaGGWP <arya.lampung.2016@gmail.com>
2025-03-17 19:44:10 +0700

    create loop do while with SwitchCase

--- ContohSwitchCase.cpp
+++ ContohSwitchCase.cpp
@@ -1,2 +1,47 @@
 #include <iostream>
 using namespace std;
+
+float luasPersegiPanjang(float a, float b) {
+    return a * b;
+}
+
+float luasLingkaran(float a) {
+    return 3.14 * a * a;
+}
+
+int main() {
+    int pilihan;
+    float panjang, lebar;
+    float jejari;
+
+    do{
+        cout << "Menu" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Keluar" << endl;
+        cout << "Pilihan (1/2/3) = ";
+        cin >> pilihan;
+
+        switch (pilihan) 
+        {   
+            case 1:
+                cout << "Hitung Luas Persegi Panjang"<< endl;
+                cout << "Masukkan panjang = ";
+                cin >> panjang;
+                cout << "Masukkan lebar = ";
+                cin >> lebar;
+                cout << "Luas Persegi Panjang = " << luasPersegiPanjang(panjang, lebar) << endl;
+                break;
+            case 2:
+                cout << "Hitung Luas Lingkaran"<< endl;
+                cout << "Masukkan jejari = ";
+                cin >> jejari;
+                cout << "Luas Lingkaran = " << luasLingkaran(jejari) << endl;
+                break;
+            case 3:
+                break;
+            default:
+                cout << "Pilihan salah, pilihan harus antara 1-3" << endl;        
+        }
+    } while (pilihan !=3);
+}
